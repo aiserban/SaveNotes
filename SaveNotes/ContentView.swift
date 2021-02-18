2f08ddd209cd3c52bda4a984bf2ddfcbf4d41f23
Andrei Serban <aiserban@outlook.com>
2021-02-18 16:19:39 +0200

    made both add and edit buttons show up

--- SaveNotes/ContentView.swift
+++ SaveNotes/ContentView.swift
@@ -10,35 +10,46 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                }.onDelete(perform: deleteItems)
             }
-            .onDelete(perform: deleteItems)
+            .toolbar {
+                //                #if os(iOS)
+                ToolbarItem(placement: .primaryAction, content: ({
+                    Button(action: addItem) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }))
+                ToolbarItem(placement: .bottomBar, content: ({
+                    EditButton()
+                }))
+                
+            }.navigationBarTitle("Notes")
+            .navigationBarBackButtonHidden(true)
+            //                #endif
+            
         }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
+        
+        
     }
-
+    
+    
+    
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -49,11 +60,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
