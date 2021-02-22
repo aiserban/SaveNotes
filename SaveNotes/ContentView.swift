f8523c61da521b3d51aa6c618b42f157916698e7
Andrei Serban <aiserban@outlook.com>
2021-02-22 20:50:50 +0200

    Working add notes

--- SaveNotes/ContentView.swift
+++ SaveNotes/ContentView.swift
@@ -12,30 +12,28 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        sortDescriptors: [NSSortDescriptor(keyPath: \Note.timestamp, ascending: true)],
         animation: .default)
-    private var items: FetchedResults<Item>
+    private var notes: FetchedResults<Note>
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
-                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                }.onDelete(perform: deleteItems)
+                ForEach(notes) { note in
+                    Text("Item at \(note.timestamp!, formatter: itemFormatter)")
+                }.onDelete(perform: deleteNotes)
             }
             .toolbar {
                 //                #if os(iOS)
                 ToolbarItem(placement: .primaryAction, content: ({
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                    NavigationLink("Add", destination: CreateNote())
                 }))
                 ToolbarItem(placement: .bottomBar, content: ({
                     EditButton()
                 }))
                 
             }.navigationBarTitle("Notes")
-            .navigationBarBackButtonHidden(true)
+//            .navigationBarBackButtonHidden(true)
             //                #endif
             
         }
@@ -43,27 +41,10 @@ struct ContentView: View {
         
     }
     
-    
-    
-    private func addItem() {
+
+    private func deleteNotes(offsets: IndexSet) {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            offsets.map { notes[$0] }.forEach(viewContext.delete)
             
             do {
                 try viewContext.save()
